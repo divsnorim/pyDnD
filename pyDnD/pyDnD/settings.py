0fc53be7ca4f2df5187c69116bbe329c2e83d83c
Aliaksei T. <mumiayr80@gmail.com>
2024-08-28 00:29:59 +0300

    add study package skibidi_app, rework settings.py

--- pyDnD/pyDnD/settings.py
+++ pyDnD/pyDnD/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cocojumbo',
+    'cocojumbo.apps.CocojumboConfig',
+    'skibidi_app.apps.SkibidiAppConfig',
 ]
 
 MIDDLEWARE = [
